cec6ea43a6d91a16a87ddb905a47917798d0ffbe
ta45668 <55038601+ta45668@users.noreply.github.com>
2021-04-20 20:02:44 +0800

    20210420
    增加上課素材
    增加鎖頭的UI

---
+++ GameTest/Assets/Script/ChooseLockCtrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseLockCtrl : MonoBehaviour
+{
+    [Header("密碼鎖的物件"), SerializeField]
+    GameObject[] lockObj = new GameObject[6];
+
+    int nowLockNubmer = 0;//現在所選擇的鎖
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < lockObj.Length; i++)
+        {
+            lockObj[i].SetActive(false);
+        }
+        lockObj[0].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            lockObj[nowLockNubmer].SetActive(false);
+            nowLockNubmer++;
+            if (nowLockNubmer >= 6)
+            {
+                nowLockNubmer = 0;
+            }
+            lockObj[nowLockNubmer].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            lockObj[nowLockNubmer].SetActive(false);
+            nowLockNubmer--;
+            if (nowLockNubmer <= -1)
+            {
+                nowLockNubmer = 5;
+            }
+            lockObj[nowLockNubmer].SetActive(true);
+        }
+		
+	}
+}
---
+++ 遊戲教學/程式碼/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+	public Text scoreText;
+	public static int score ;
+
+	// Use this for initialization
+	void Start () {
+		
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "SCORE : " + score.ToString();
+	}
+}
