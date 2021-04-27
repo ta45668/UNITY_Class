e2fe347e3d1c9b7abdd41131cee769f3e5521e68
ta45668 <55038601+ta45668@users.noreply.github.com>
2021-04-27 20:08:23 +0800

    教學

--- GameTest/Assets/Script/ChooseLockCtrl.cs
+++ GameTest/Assets/Script/ChooseLockCtrl.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+//駝峰式 = 單字的第一個英文字母要大寫
+//只要是腳本的第一個都是大寫
+//變數的第一個是小寫
+//全域變數的第一個是大寫 = static
+//全域變數 = 在所有的腳本都可以使用
+//功能的第一個是大寫
+
 public class ChooseLockCtrl : MonoBehaviour
 {
+    //protected =//小山貓
+    //public = 公開變數 = 可以由外部作修改即使用
+    //private = 私人變數 = 只能自己使用
+    //public static = 全域變數 = 在所有的腳本都可以使用
+    //什麼都不加 = 只能自己使用(沒有保護)
+
+    //變數 = 關鍵字+修飾詞+型態
+
+    //int = 整數
+    //float = 小數
+    //bool = 布林值
+
     [Header("密碼鎖的物件"), SerializeField]
     GameObject[] lockObj = new GameObject[6];
 
     Transform[][] lockNunbmerObj = new Transform[6][];//選擇的物件
     Vector3[][] lockNunbmerPost = new Vector3[6][];//選擇物件的原始位置
-
+    
     int nowLockNubmer = 0;//現在所選擇的鎖
     int[] nowLockNubmerPost = new int[6] { 0, 0, 0, 0, 0, 0 };//現在所選擇的鎖位置(答案)
     bool isChoose = false;//正在選擇密碼
@@ -82,7 +101,7 @@ public class ChooseLockCtrl : MonoBehaviour
     }
 
     /// <summary>
-    /// 移動物件方法
+    /// 移動物件(LockNunbmer)方法
     /// </summary>
     /// <param name="reelNumber">滾輪編號</param>
     /// <param name="intervalNumber">區間數字(防止超出時會有破綻)</param>
